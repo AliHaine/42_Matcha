4d3c5c72dbda22d9559edfaeb58a8cbff41a26dc
AliHaine <alitr57120@gmail.com>
2025-01-08 17:09:09 +0100

    UPdate homepage and card comp for testing

--- angular-frontend/src/app/home/home.component.ts
+++ angular-frontend/src/app/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, inject, ViewChild, ViewContainerRef} from '@angular/core';
+import {CardComponent} from "../card/card.component";
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-root',
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  @ViewChild('CardComponent', { read: ViewContainerRef, static: true })
+  private container!: ViewContainerRef;
 
+  public injectTest() {
+    const cardRef = this.container.createComponent(CardComponent);
+    console.log('Card injected:', cardRef);
+}
 }
