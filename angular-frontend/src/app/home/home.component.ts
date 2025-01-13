a59c209d08214e6ed16429cadb2d5a5794617d3d
AliHaine <alitr57120@gmail.com>
2025-01-13 14:43:56 +0100

    Navbar setup

--- angular-frontend/src/app/home/home.component.ts
+++ angular-frontend/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import {Component, inject, ViewChild, ViewContainerRef} from '@angular/core';
 import {CardComponent} from "../card/card.component";
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CardComponent],
+  imports: [CardComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
