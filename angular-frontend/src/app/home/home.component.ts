62ad1eaeeb5d463f3576ae737d636bf3d24c2107
AliHaine <alitr57120@gmail.com>
2025-01-14 11:05:00 +0100

    Minor fix in nav, footer setup

--- angular-frontend/src/app/home/home.component.ts
+++ angular-frontend/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import {Component, inject, ViewChild, ViewContainerRef} from '@angular/core';
 import {CardComponent} from "../card/card.component";
 import {NavbarComponent} from "../navbar/navbar.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CardComponent, NavbarComponent],
+  imports: [CardComponent, NavbarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
