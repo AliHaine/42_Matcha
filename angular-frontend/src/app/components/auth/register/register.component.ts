03b373f56292b49ead2e9d358f587b8439b48f9a
AliHaine <alitr57120@gmail.com>
2025-01-29 23:16:37 +0100

    Update register  comp

--- angular-frontend/src/app/components/auth/register/register.component.ts
+++ angular-frontend/src/app/components/auth/register/register.component.ts
@@ -2,17 +2,19 @@ import {Component, inject} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {RouterLink} from "@angular/router";
 import {ApiService} from "../../../services/api.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, RouterLink],
+    imports: [ReactiveFormsModule, RouterLink, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+    currentStep: number = 0;
     apiService = inject(ApiService);
-    formControlGroup = new FormGroup({
+    formControlGroupStep1 = new FormGroup({
         lastname: new FormControl('', Validators.required),
         firstname: new FormControl('', Validators.required),
         email: new FormControl('', Validators.required),
@@ -22,9 +24,32 @@ export class RegisterComponent {
         gender: new FormControl('male', Validators.required)
     });
 
-    submit(event: Event) {
+    formControlGroupStep2 = new FormGroup({
+        city: new FormControl('', Validators.required),
+        searching: new FormControl('', Validators.required),
+        commitment: new FormControl('', Validators.required),
+        frequency: new FormControl('', Validators.required),
+
+        weight: new FormControl('', Validators.required),
+        size: new FormControl('', Validators.required),
+        shape: new FormControl('', Validators.required),
+
+        smoking: new FormControl('', Validators.required),
+        alcohol: new FormControl('', Validators.required),
+        diet: new FormControl('', Validators.required),
+    });
+
+    formControlGroupStep3 = new FormGroup({
+        artCulture: new FormControl('', Validators.required),
+        sportActivity: new FormControl('', Validators.required),
+        other: new FormControl('', Validators.required),
+        description: new FormControl('', Validators.required),
+    });
+
+    submit(event: Event, values: any) {
       event.preventDefault();
-      console.log(this.formControlGroup.value);
-      this.apiService.postData('/account/register', this.formControlGroup.value);
+      this.currentStep++;
+      console.log(values);
+      this.apiService.postData('/account/register', values);
     }
 }
