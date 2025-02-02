15e7aea49ea88849cebbf5aab3477522250cf6da
AliHaine <alitr57120@gmail.com>
2025-02-02 20:34:43 +0100

    Update serach page and home page

--- angular-frontend/src/app/components/search/search.component.ts
+++ angular-frontend/src/app/components/search/search.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {CardService} from "../../services/card.service";
+import {CardComponent} from "../card/card/card.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [
+    CardComponent,
+    NgForOf
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
 
+  cardService = inject(CardService)
   activeFilter = []
 
   addFilter() {
