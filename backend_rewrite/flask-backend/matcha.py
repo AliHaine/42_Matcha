0b90a625ec18c26bcb888718ce87cbbd2b9c9b27
Axel Kastler <akastler@k2r1p8.42mulhouse.fr>
2025-02-05 15:41:29 +0100

    passing matcha to get, and query params

--- backend_rewrite/flask-backend/matcha.py
+++ backend_rewrite/flask-backend/matcha.py
@@ -11,20 +11,26 @@ from .decorators import registration_completed
 
 bp = Blueprint('matcha', __name__, url_prefix='/api/matcha')
 
-@bp.route('', methods=['POST'])
+@bp.route('', methods=['GET'])
 @jwt_required()
 @registration_completed
 def matcha():
-    try:
-        data = request.json
-    except Exception as e:
-        return jsonify({'success': False, 'error': 'Invalid parameters'})
-    if data is None:
-        return jsonify({'success': False, 'error': 'Invalid parameters'})
-    if 'nb_profiles' not in data:
-        return jsonify({'success': False, 'error': 'Missing parameters'})
+    # try:
+    #     data = request.json
+    # except Exception as e:
+    #     return jsonify({'success': False, 'error': 'Invalid parameters'})
+    # if data is None:
+    #     return jsonify({'success': False, 'error': 'Invalid parameters'})
+    # if 'nb_profiles' not in data:
+    #     return jsonify({'success': False, 'error': 'Missing parameters'})
     db = get_db()
-    nb_profiles = data.get('nb_profiles', 1)
+    nb_profiles = request.args.get('nb_profiles', 1)
+    if isinstance(nb_profiles, str):
+        try:
+            print("\n\n\ntry to convert",nb_profiles end="\n\n\n\n")
+            nb_profiles = int(nb_profiles)
+        except Exception as e:
+            return jsonify({'success': False, 'error': 'Invalid parameters'})
     user = None
     with db.cursor() as cur:
         cur.execute('SELECT * FROM users WHERE email = %s', (get_jwt_identity(),))
