9629718a155221258bd769feb4b5bbe708425655
ChromaXard <axel.kastler6847@gmail.com>
2025-02-11 13:47:39 +0100

    removing 401 return when an error occurs on the jwt token

--- backend_rewrite/flask-backend/jwt_handler.py
+++ backend_rewrite/flask-backend/jwt_handler.py
@@ -1,13 +1,13 @@
 from flask import jsonify
 
 def missing_token_callback(error):
-    return jsonify({"success": False, "Error": "Please provide your token, or authenticate you to get one"}), 401
+    return jsonify({"success": False, "Error": "Please provide your token, or authenticate you to get one"})
 
 def expired_token_callback(jwt_header, jwt_payload):
-    return jsonify({"success": False, "Error": "Token expired. Please authenticate you to get a new one"}), 401
+    return jsonify({"success": False, "Error": "Token expired. Please authenticate you to get a new one"})
 
 def invalid_token_callback(error):
-    return jsonify({"success": False, "Error": "Invalid token. Please authenticate you to get a new one"}), 401
+    return jsonify({"success": False, "Error": "Invalid token. Please authenticate you to get a new one"})
 
 def revoked_token_callback(jwt_header, jwt_payload):
-    return jsonify({"success": False, "Error": "Token revoked. Please authenticate you to get a new one"}), 401
+    return jsonify({"success": False, "Error": "Token revoked. Please authenticate you to get a new one"})
