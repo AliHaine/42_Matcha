a31f1c5b1693cac0ec010e5a3c977497dea1be2a
AliHaine <alitr57120@gmail.com>
2025-02-12 14:31:14 +0100

    Setup chatroom

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -6,10 +6,12 @@ import {RegisterComponent} from "./components/auth/register/register.component";
 import {SearchComponent} from "./components/search/search.component";
 import {ProfileComponent} from "./components/profile/profile.component";
 import {authGuard} from "./others/auth.guard";
+import {ChatComponent} from "./components/chat/chat.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [authGuard] },
     { path: 'page/:name', component: PageComponent },
+    { path: 'chat', component: ChatComponent, canActivate: [authGuard] },
     { path: 'search', component: SearchComponent, canActivate: [authGuard] },
     { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
     { path: 'profile/:id', component: ProfileComponent, canActivate: [authGuard] },
