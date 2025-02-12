d9532ffada570587418a78fd4e76d0adaaed6f3c
AliHaine <alitr57120@gmail.com>
2025-02-12 01:08:59 +0100

    Update the login page to display an error message if an error happen

--- angular-frontend/src/app/components/auth/login/login.component.ts
+++ angular-frontend/src/app/components/auth/login/login.component.ts
@@ -3,10 +3,11 @@ import {Router, RouterLink} from "@angular/router";
 import {ApiService} from "../../../services/api.service";
 import {EmailValidator, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AuthService} from "../../../services/auth.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, ReactiveFormsModule],
+    imports: [RouterLink, ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -15,6 +16,7 @@ export class LoginComponent {
     authService = inject(AuthService);
     apiService = inject(ApiService);
     router = inject(Router)
+    errorMessage: string = "";
     formControlGroup = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', Validators.required),
@@ -24,13 +26,14 @@ export class LoginComponent {
     submit(event: Event) {
         event.preventDefault();
         this.apiService.postData("/auth/login", this.formControlGroup.value).subscribe(res => {
-        if (res['success']) {
-            this.apiService.saveAccessToken(res['access_token']);
-            this.authService.login();
-            this.router.navigate([''])
-        } else {
-            console.log(res);
-        }
+            if (res['success']) {
+                this.apiService.saveAccessToken(res['access_token']);
+                this.authService.login();
+                this.router.navigate(['']);
+            } else {
+                this.formControlGroup.get("password")?.setValue("");
+                this.errorMessage = res['error'];
+            }
       });
     }
 }
