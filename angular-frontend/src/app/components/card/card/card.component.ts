d396f6740c01317dae027de3abdcf2d48591d20f
AliHaine <alitr57120@gmail.com>
2025-02-13 20:27:20 +0100

    Upadte profile to match specific user

--- angular-frontend/src/app/components/card/card/card.component.ts
+++ angular-frontend/src/app/components/card/card/card.component.ts
@@ -1,11 +1,13 @@
 import {Component, input, InputSignal} from '@angular/core';
 import {CardModel} from "../../../models/card.model";
 import {InterestComponent} from "../interest/interest.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-card',
   imports: [
-    InterestComponent
+    InterestComponent,
+    RouterLink
   ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
--- angular-frontend/src/app/components/profile/profile.component.ts
+++ angular-frontend/src/app/components/profile/profile.component.ts
@@ -1,6 +1,8 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {ProfileModel} from "../../models/profile.model";
 import {ApiService} from "../../services/api.service";
+import {ActivatedRoute} from "@angular/router";
+import {switchMap} from "rxjs";
 
 @Component({
   selector: 'app-profile',
@@ -8,10 +10,11 @@ import {ApiService} from "../../services/api.service";
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
   lorem: string = "Auxerunt haec vulgi sordidioris audaciam, quod cum ingravesceret penuria commeatuum, famis et furoris inpulsu Eubuli cuiusdam inter suos clari domum ambitiosam ignibus subditis inflammavit rectoremque ut sibi iudicio imperiali addictum calcibus incessens et pugnis conculcans seminecem laniatu miserando discerpsit. post cuius lacrimosum interitum in unius exitio quisque imaginem periculi sui considerans documento recenti similia formidabat.Post haec indumentum regale quaerebatur et ministris fucandae purpurae tortis confessisque pectoralem tuniculam sine manicis textam, Maras nomine quidam inductus est ut appellant Christiani diaconus, cuius prolatae litterae scriptae Graeco sermone ad Tyrii textrini praepositum celerari speciem perurgebant quam autem non indicabant denique etiam idem ad usque discrimen vitae vexatus nihil fateri conpulsus est. non indicabant denique etiam idem ad usque discrimen vitae vexatus nihil fateri conpulsus est. non indicabant denique etiam idem ad usque discrimen vitae vexatus nihil fateri conpulsus est."
   apiService = inject(ApiService);
+  route = inject(ActivatedRoute);
   profile = signal<ProfileModel>(new ProfileModel({}));
 
   constructor() {
@@ -33,4 +36,8 @@ export class ProfileComponent {
 
     //this.profile.set(new ProfileModel(this.apiService.get());
   }
+
+  ngOnInit(): void {
+      console.log(this.route.paramMap);
+    }
 }
