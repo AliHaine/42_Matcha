a9d5557a6d23bfcc035912831ddc6a9ab9d48a4b
AliHaine <alitr57120@gmail.com>
2025-02-21 03:14:51 +0100

    Setup notification sys

--- angular-frontend/src/app/components/navbar/navbar.component.ts
+++ angular-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NotificationComponent} from "../notification/notification.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, NotificationComponent, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  showNotif: boolean = false;
+
   overtest() {
+    this.showNotif = true;
     console.log("hover")
   }
 }
