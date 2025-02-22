8e0a0d44400e6b0902e01a6429c392bddd1bc43f
AliHaine <alitr57120@gmail.com>
2025-02-22 01:53:07 +0100

    Update account page input have the right type. Remove unused styles

--- angular-frontend/src/app/components/account/account.component.ts
+++ angular-frontend/src/app/components/account/account.component.ts
@@ -2,11 +2,22 @@ import {Component, inject} from '@angular/core';
 import {ApiService} from "../../services/api.service";
 import {AuthService} from "../../services/auth.service";
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
+import {LocationService} from "../../services/location.service";
+import {CdkTextareaAutosize} from "@angular/cdk/text-field";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {TextFieldModule} from '@angular/cdk/text-field';
 
 @Component({
   selector: 'app-account',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgForOf,
+    CdkTextareaAutosize,
+    MatFormField,
+    MatInput,
+    TextFieldModule
   ],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
@@ -15,19 +26,35 @@ export class AccountComponent {
 
   apiService = inject(ApiService);
   authService = inject(AuthService);
+  locationService = inject(LocationService);
   formGroup: FormGroup = new FormGroup({
-    firstname: new FormControl('sa'),
-  })
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    city: new FormControl(''),
+    age: new FormControl(''),
+    gender: new FormControl(''),
+    hetero: new FormControl(''),
+    interests: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    description: new FormControl(''),
+  } as {[key: string]: any});
 
   constructor() {
     this.apiService.getData("/profiles/me", {}).subscribe(result => {
-      //this.formGroup.controls
-      console.log(result)
+      console.log(this.formGroup.value["city"])
+      for (const value in result["user"])
+        if (this.formGroup.value[value] !== undefined)
+          this.formGroup.controls[value].setValue(result["user"][value]);
     })
+    this.locationService.observableToSubscribe(this.formGroup.controls["city"].valueChanges)
   }
 
   applyTrigger() {
-
+    console.log(this.formGroup.value)
   }
 
+  setLocation(name: string) {
+    this.formGroup.controls["city"].setValue(name);
+  }
 }
--- angular-frontend/src/app/components/auth/register/register.component.ts
+++ angular-frontend/src/app/components/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import {Component, effect, inject, signal} from '@angular/core';
+import {Component, effect, inject} from '@angular/core';
 import {
     AbstractControl, FormArray,
     FormControl,
@@ -14,10 +14,13 @@ import {NgForOf, NgIf} from "@angular/common";
 import {RegisterService} from "../../../services/register.service";
 import {AuthService} from "../../../services/auth.service";
 import {LocationService} from "../../../services/location.service";
+import {TextFieldModule} from '@angular/cdk/text-field';
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
     selector: 'app-register',
-    imports: [ReactiveFormsModule, RouterLink, NgIf, NgForOf],
+    imports: [ReactiveFormsModule, RouterLink, NgIf, NgForOf, TextFieldModule, MatFormField, MatInput],
     templateUrl: './register.component.html',
     styleUrl: './register.component.css'
 })
