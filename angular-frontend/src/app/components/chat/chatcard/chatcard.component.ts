6dd2c0ef5d688690185974b7b3f26c93a95386e5
AliHaine <alitr57120@gmail.com>
2025-03-06 12:53:03 +0100

    Chat model add, update chat card to work with a chat model

--- angular-frontend/src/app/components/chat/chatcard/chatcard.component.ts
+++ angular-frontend/src/app/components/chat/chatcard/chatcard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, input, InputSignal} from '@angular/core';
+import {ChatModel} from "../../../models/chat.model";
 
 @Component({
   selector: 'app-chatcard',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './chatcard.component.css'
 })
 export class ChatcardComponent {
-
+  chatModel: InputSignal<ChatModel> = input.required();
 }
--- angular-frontend/src/app/models/chat.model.ts
+++ angular-frontend/src/app/models/chat.model.ts
@@ -1,3 +1,18 @@
 export class ChatModel {
+    firstname: string;
+    age: number;
+    city: string;
+    status: boolean;
+    lastMsgTime: string;
+    lastMessage: string;
+    messages: string[] = [];
 
+    constructor(data: {[key: string]: any}) {
+        this.firstname = data['firstname'];
+        this.age = data['age'];
+        this.city = data['city'];
+        this.status = data['status'];
+        this.lastMsgTime = data['lastMsgTime'];
+        this.lastMessage = data['lastMessage'];
+    }
 }
