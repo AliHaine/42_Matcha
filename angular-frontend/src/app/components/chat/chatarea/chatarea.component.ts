1a52e4c23f97e3014ffb8e752040e737737c2e1d
AliHaine <alitr57120@gmail.com>
2025-03-06 21:14:02 +0100

    Chat area css and html update

--- angular-frontend/src/app/components/chat/chatarea/chatarea.component.ts
+++ angular-frontend/src/app/components/chat/chatarea/chatarea.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-chatarea',
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './chatarea.component.html',
   styleUrl: './chatarea.component.css'
 })
