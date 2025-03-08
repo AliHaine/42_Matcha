9694704ca2d34c74d29d1d4b8c1276d09d78dab0
AliHaine <alitr57120@gmail.com>
2025-03-08 13:23:54 +0100

    setup message sender

--- angular-frontend/src/app/components/chat/chatarea/chatarea.component.ts
+++ angular-frontend/src/app/components/chat/chatarea/chatarea.component.ts
@@ -1,15 +1,23 @@
 import {Component, input, InputSignal} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {ChatModel} from "../../../models/chat.model";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-chatarea',
   imports: [
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './chatarea.component.html',
   styleUrl: './chatarea.component.css'
 })
 export class ChatareaComponent {
   chatModel: InputSignal<ChatModel> = input.required();
+  messageInput = new FormControl('');
+
+  sendMessage(event: Event) {
+    event.preventDefault();
+    this.messageInput.setValue('');
+  }
 }
