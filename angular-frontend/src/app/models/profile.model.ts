a4828df7b8d626304fa4954e7f1b14f14bdd0c91
AliHaine <alitr57120@gmail.com>
2025-04-05 15:38:04 +0200

    Display the score of user in home page and add icon for score

--- angular-frontend/src/app/models/profile.model.ts
+++ angular-frontend/src/app/models/profile.model.ts
@@ -19,6 +19,7 @@ export class ProfileModel {
     userId: number;
     fameRate: number;
     hetero: boolean;
+    score: number;
 
     constructor(data: {[key: string]: any}) {
         this.email = data["email"];
@@ -44,6 +45,7 @@ export class ProfileModel {
         this.userId = data["id"];
         this.fameRate = data["fame_rate"];
         this.hetero = data["hetero"];
+        this.score = data["score"];
     }
 
     dumpAsDict(): {[key: string]: any} {
