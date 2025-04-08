c60009e3b5089378b24bbf40db0c93de8841d2da
AliHaine <alitr57120@gmail.com>
2025-04-09 00:04:03 +0200

    Page compo use now onPusg start, no more ngif in root component

--- angular-frontend/src/app/components/root.component.ts
+++ angular-frontend/src/app/components/root.component.ts
@@ -3,7 +3,6 @@ import {RouterOutlet} from "@angular/router";
 import {FooterComponent} from "./footer/footer.component";
 import {NavbarComponent} from "./navbar/navbar.component";
 import {AuthService} from "../services/auth.service";
-import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -11,10 +10,9 @@ import {NgIf} from "@angular/common";
     RouterOutlet,
     FooterComponent,
     NavbarComponent,
-    NgIf
   ],
   template: `<div id="root-content">
-              <app-navbar *ngIf="this.authService.isLoggedIn()"></app-navbar>
+              <app-navbar></app-navbar>
               <div style="flex: 1; display: flex; align-items: center; justify-content: center; flex: 1;">
                 <router-outlet></router-outlet>
               </div>
