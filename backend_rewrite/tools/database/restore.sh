46c892e3e35da3901889f6f6929d9c5b6e15b137
ChromaXard <axel.kastler6847@gmail.com>
2025-04-30 13:36:12 +0200

    fixing database restoring on some cases

--- backend_rewrite/tools/database/restore.sh
+++ backend_rewrite/tools/database/restore.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 echo "Restoring dump into database..."
-pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/sauvegarde.dump
+createdb "$POSTGRES_USER" -U "$POSTGRES_USER"
+pg_restore --no-owner -U "$POSTGRES_USER" -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/sauvegarde.dump
