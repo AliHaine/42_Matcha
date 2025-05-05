5805bdba40d3daed61e290045cee8ba302d81d93
ChromaXard <axel.kastler6847@gmail.com>
2025-05-05 09:50:10 +0200

    removing create db from restore.sh and change healthcheck test of postgres

--- backend_rewrite/tools/database/restore.sh
+++ backend_rewrite/tools/database/restore.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 echo "Restoring dump into database..."
-createdb "$POSTGRES_USER" -U "$POSTGRES_USER"
 pg_restore --no-owner -U "$POSTGRES_USER" -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/sauvegarde.dump
